a583df3ee2aad4b46b9d3ac37be3795d4e8f0069
yeounoh <yeounohster@gmail.com>
2017-10-27 16:28:34 -0400

    slicing implemented

--- slice_finder.py
+++ slice_finder.py
@@ -10,10 +10,35 @@
 """
 
 import numpy as np
+import copy
+from scipy import stats
+from risk_control import *
 
+"""
+    Slice is specified with a dictionary that maps a set of attributes 
+    and their values. For instance, '1 <= age < 5' is expressed as {'age':[1,5]}
+    and 'gender = male' as {'gender':['male']}
+"""
 class Slice:
-    def __init__(self):
+    def __init__(self, filters, data):
+        self.filters = filters
+        self.data = data
+
+    def get_filter(self):
+        return self.filters
+
+    def set_filter(self, filters):
+        self.filters = filters
+
+    def union(self, s):
+        ''' union with Slice s '''
         pass
+
+    def __str__(self):
+        slice_desc = ''
+        for k, v in self.filters.iteritems():
+            slice_desc += '%s:%s '%(k,v)
+        return slice_desc 
 
 class SliceFinder:
     def __init__(self):
@@ -21,9 +46,29 @@ class SliceFinder:
 
     def slicing(self, X, y):
         ''' Generate base slices '''
+        n, m = X.shape[0], X.shape[1]
 
+        base_slices = []
         for col in X.columns:
-            print np.unique(X[col])
+            uniques, counts = np.unique(X[col], return_counts=True)
+            if len(uniques) == n:
+                # Skip ID like col
+                continue
+            if len(uniques) > n/2.:
+                # Bin high cardinality col
+                bin_edges = binning(X[col], n_bin=10)
+                for i in range(len(bin_edges)-1):
+                    ta = bin_edges[i] <= X[col]
+                    s = Slice({col:[bin_edges[i],bin_edges[i+1]]},
+                              (X[ np.logical_and(bin_edges[i] <= X[col], X[col] < bin_edges[i+1]) ],
+                               y[ np.logical_and(bin_edges[i] <= X[col], X[col] < bin_edges[i+1]) ] ))
+                    base_slices.append(s)
+            else:
+                for v in uniques:
+                    s = Slice({col:[v]}, (X[X[col] == v], y[X[col] == v]))                 
+                    base_slices.append(s)
+
+        return base_slices
         
 
     def filter_by_effect_size(self, slices, epsilon):
@@ -40,4 +85,7 @@ class SliceFinder:
         pass
 
 
-    
+def binning(col, n_bin=10):
+    ''' Equi-height binning '''
+    bin_edges = stats.mstats.mquantiles(col, np.arange(0., 1.+1./n_bin, 1./n_bin))
+    return bin_edges
