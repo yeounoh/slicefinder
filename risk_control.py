bug fix in effect size calculation

risk_control.py
@@ -5,6 +5,7 @@
 """
 from scipy import stats
 import numpy as np
+import math
 
 
 
@@ -12,19 +13,17 @@ def t_testing(sample_a, sample_b, alpha=0.05):
     t, prob = stats.ttest_ind(sample_a, sample_b)
     return prob <= alpha
 
-def t_testing(sample_a, alpha=0.05):
+def t_testing(sample_a, reference, alpha=0.05):
+    mu, s, n = reference[0], reference[1], reference[2]
+    sample_b_mean = (mu*n - np.sum(sample_a))/(n-len(sample_a))
+    sample_b_var = (s**2*(n-1) - np.std(sample_a)**2*(len(sample_a)-1))/(n-len(sample_a)-1)
     pass
-
-def effect_size(sample_a, sample_b):
-    diff = np.mean(sample_a) - np.mean(sample_b) 
-    diff /= (np.std(sample_a)+np.std(sample_b))/2.
-    return diff
 
 def effect_size(sample_a, reference):
     mu, s, n = reference[0], reference[1], reference[2]
     sample_b_mean = (mu*n - np.sum(sample_a))/(n-len(sample_a))
-    sample_b_std = (s**2*(n-1) - np.std(sample_a)**2*(len(sample_a)-1))/(n-len(sample_a)-1)
+    sample_b_var = (s**2*(n-1) - np.std(sample_a)**2*(len(sample_a)-1))/(n-len(sample_a)-1)
 
     diff = np.mean(sample_a) - sample_b_mean
-    diff /= (np.std(sample_a) + sample_b_std)/2.
+    diff /= (np.std(sample_a) + math.sqrt(sample_b_var))/2.
     return diff

slice_finder_test.py
@@ -57,7 +57,7 @@ class test_slice_finder(unittest.TestCase):
         metrics_all = sf.evaluate_model((X, y))
         reference = (np.mean(metrics_all), np.std(metrics_all), len(metrics_all))
         base_slices = sf.slicing(X, y)
-        filtered_slices = sf.filter_by_effect_size(base_slices, reference, epsilon=0.5)
+        filtered_slices = sf.filter_by_effect_size(base_slices, reference, epsilon=0.2)
         print (len(base_slices), len(filtered_slices))
 
     def test_alpha_investing(self):